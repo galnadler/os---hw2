193cec1cfdd6a47169d953f4753928ac384fd250
galnadler <128317396+galnadler@users.noreply.github.com>
2024-07-02 23:07:07 +0300

    hi7

--- myshell.c
+++ myshell.c
@@ -20,36 +20,39 @@ int prepare(void);
 int handle_signal(int signum, void (*action)(int));
 int finalize(void);
 
-//
-//
-
+// arglist - a list of char* arguments (words) provided by the user
+// it contains count+1 items, where the last item (arglist[count]) and *only* the last is NULL
+// RETURNS - 1 if should continue, 0 otherwise
 int process_arglist(int count, char **arglist)
 {
-	printf("Debug: process_arglist called with %d arguments.\n", count);
+
 	if (count == 1)
 	{
 		return execute_general(count, arglist);
 	}
 	for (int i = 0; i < count; i++)
 	{
-		printf("Debug: arglist[%d] = %s\n", i, arglist[i]);
 		if (strcmp(arglist[i], "|") == 0)
 		{
+			// run two child processes, with the output of the first process piped to the input of the second process.
 			return pipe_it_up(count, arglist, i);
 		}
 
 		if (strcmp(arglist[i], "&") == 0)
 		{
+			// run the child process in the background
 			return run_process_background(count, arglist);
 		}
 
 		if (count >= 2 && (strcmp(arglist[i], "<") == 0))
 		{
+			// open the specified file and then run the child process, with the input redirected from the input file.
 			return open_child_process_input(count, arglist);
 		}
 
 		if (count > 1 && (strcmp(arglist[i], ">>") == 0))
 		{
+			// open the specified file and then run the child process, with the output redirected from the output file.
 			return open_child_process_output(count, arglist);
 		}
 	}
@@ -58,7 +61,6 @@ int process_arglist(int count, char **arglist)
 
 int run_process_background(int count, char **arglist)
 {
-	printf("Debug: run_process_background called.\n");
 
 	pid_t pid;
 	pid = fork();
@@ -68,7 +70,7 @@ int run_process_background(int count, char **arglist)
 	}
 	if (pid == 0)
 	{
-		printf("Debug: Child process (background) created.\n");
+		// Child process
 		if (handle_signal(SIGCHLD, SIG_DFL) == 1)
 		{
 			raise_error("Error - Could not change signal handling");
@@ -84,7 +86,6 @@ int run_process_background(int count, char **arglist)
 
 int pipe_it_up(int count, char **arglist, int i)
 {
-	printf("Debug: pipe_it_up called.\n");
 
 	pid_t pid1, pid2;
 	int pipefd[2];
@@ -100,20 +101,22 @@ int pipe_it_up(int count, char **arglist, int i)
 		perror("Failed during forking");
 		return 0;
 	}
+	// Child process - 1st child.
 	if (pid1 == 0)
 	{
-		printf("Debug: First child process (pipe) created.\n");
+
 		close(pipefd[0]);
 		if (handle_signal(SIGINT, SIG_DFL) + handle_signal(SIGCHLD, SIG_DFL) > 0)
 		{
 			raise_error("Error");
 		}
+		// Stdout to pipe
 		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
 		{
 			raise_error("Error - Could not redirect stdout of child process");
 		}
-		close(pipefd[1]);
-		arglist[i] = NULL;
+		close(pipefd[1]);  // Close Write end
+		arglist[i] = NULL; // Split arglist
 		if (execvp(arglist[0], arglist) == -1)
 		{
 			raise_error("Error - while executing command");
@@ -126,28 +129,31 @@ int pipe_it_up(int count, char **arglist, int i)
 		perror("Failed during forking");
 		return 0;
 	}
+	// Child process - 2nd child.
 	if (pid2 == 0)
 	{
-		printf("Debug: Second child process (pipe) created.\n");
+
 		close(pipefd[1]);
 		if (handle_signal(SIGINT, SIG_DFL) + handle_signal(SIGCHLD, SIG_DFL) > 0)
 		{
 			raise_error("Error");
 		}
+		// Stdin to pipe
 		if (dup2(pipefd[0], STDIN_FILENO) == -1)
 		{
 			raise_error("Error - Could not redirect stdin of child process");
 		}
-		close(pipefd[0]);
+		close(pipefd[0]); // Close Read end
 		if (execvp(arglist[i + 1], &arglist[i + 1]) == -1)
 		{
 			raise_error("Error - Could not complete executing command");
 		}
 	}
-
+	// Close both ends of the pipe (parent)
 	close(pipefd[0]);
 	close(pipefd[1]);
 
+	// Wait for both child processes to finish
 	if (waitpid(pid1, NULL, 0) == -1 && errno != ECHILD && errno != EINTR)
 	{
 		perror("failure during waitpid");
@@ -164,8 +170,7 @@ int pipe_it_up(int count, char **arglist, int i)
 
 int open_child_process_input(int count, char **arglist)
 {
-	printf("Debug: open_child_process_input called.\n");
-
+	// Input
 	pid_t pid;
 	int input_file;
 	arglist[count - 2] = NULL;
@@ -174,9 +179,10 @@ int open_child_process_input(int count, char **arglist)
 	{
 		raise_error("Failed during forking");
 	}
+	// Child process
 	if (pid == 0)
 	{
-		printf("Debug: Child process (input redirection) created.\n");
+
 		if ((handle_signal(SIGINT, SIG_DFL) + handle_signal(SIGCHLD, SIG_DFL) > 0))
 		{
 			raise_error("Error - Could not change signal handling");
@@ -210,8 +216,7 @@ int open_child_process_input(int count, char **arglist)
 
 int open_child_process_output(int count, char **arglist)
 {
-	printf("Debug: open_child_process_output called.\n");
-
+	// Output
 	pid_t pid;
 	int output_file;
 	arglist[count - 2] = NULL;
@@ -220,9 +225,10 @@ int open_child_process_output(int count, char **arglist)
 	{
 		raise_error("Failed during forking");
 	}
+	// Child process
 	if (pid == 0)
 	{
-		printf("Debug: Child process (output redirection) created.\n");
+
 		if (handle_signal(SIGINT, SIG_DFL) + handle_signal(SIGCHLD, SIG_DFL) > 0)
 		{
 			raise_error("Error - Could not change signal handling");
@@ -256,16 +262,16 @@ int open_child_process_output(int count, char **arglist)
 
 int execute_general(int count, char **arglist)
 {
-	printf("Debug: execute_general called.\n");
-
+	// Executes command and starts another one only after it is completed.
 	pid_t pid = fork();
 	if (pid == -1)
 	{
 		raise_error("Failed during forking");
 	}
+	// Child process
 	if (pid == 0)
 	{
-		printf("Debug: Child process (general) created.\n");
+
 		if (handle_signal(SIGINT, SIG_DFL) + handle_signal(SIGCHLD, SIG_DFL) > 0)
 		{
 			raise_error("Error - Could not change signal handling");
@@ -293,13 +299,13 @@ void raise_error(const char *error_type)
 
 int prepare(void)
 {
-	printf("Debug: prepare called.\n");
+
 	return handle_signal(SIGINT, SIG_IGN) + handle_signal(SIGCHLD, SIG_IGN);
 }
 
 int handle_signal(int sig, void (*to_do)(int))
 {
-	printf("Debug: handle_signal called for signal %d.\n", sig);
+	// Sets the next action to do. Returns 1 on failure, 0 on succes
 	if (signal(sig, to_do) == SIG_ERR)
 	{
 		perror("Error - Failed to change signal handling");
@@ -310,6 +316,8 @@ int handle_signal(int sig, void (*to_do)(int))
 
 int finalize(void)
 {
-	printf("Debug: finalize called.\n");
+
 	return 0;
 }
+
+// End of code :)
